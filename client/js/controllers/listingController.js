206e3ce881c2f6d6dc22e1acfc98867979e83171
Matteo <sneazels@ufl.edu>
2019-03-08 17:06:47 -0500

    Updated js controllers, factory and customer html file Item Schema

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -1,58 +1,64 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
-  function($scope, Listings) {
+angular.module('items').controller('ListingsController', ['$scope', 'Items',
+  function($scope, Items) {
     /* Get all the listings, then bind it to the scope */
     Listings.getAll().then(function(response) {
-      $scope.listings = response.data;
+      $scope.items = response.data;
     }, function(error) {
-      console.log('Unable to retrieve listings:', error);
+      console.log('Unable to retrieve items:', error);
     });
 
     $scope.detailedInfo = undefined;
+    $scope.cart = [];
 
-    $scope.addListing = function() {
+    $scope.addItem = function() {
 	  /**TODO
 	  *Save the article using the Listings factory. If the object is successfully
 	  saved redirect back to the list page. Otherwise, display the error
 	 */
-      var listing = {
-        name: $scope.newListing.name,
-        code: $scope.newListing.code,
-        address: $scope.newListing.address
+      var item = {
+        name: $scope.newItem.name,
+        code: $scope.newItem.code,
+        address: $scope.newItem.address
       };
 
-      $scope.listings.push($scope.newListing);
-      $scope.newListing = {};
+      $scope.items.push($scope.newItem);
+      $scope.newItem = {};
 
-      Listings.create(listing).then(function(response){$scope.newListing.name =''; $scope.newListing.code=''; $scope.newListing.address='';
+      Items.create(item).then(function(response){$scope.newItem.name =''; $scope.newItem.code=''; $scope.newItem.address='';
       }, function(error){
-        $scope.error = 'listing not saved\n' + error;
+        $scope.error = 'item not saved\n' + error;
       });
 
 
     };
     /** DONE WITH addListing **/
 
-    $scope.deleteListing = function(index) {
+    $scope.deleteItem = function(index) {
 	   /**TODO
         Delete the article using the Listings factory. If the removal is successful,
 		navigate back to 'listing.list'. Otherwise, display the error.
        */
 
 
-      var id = $scope.listings[index];
-      console.log('listing '+ id._id);
+      var id = $scope.items[index];
+      console.log('item '+ id._id);
 
-      Listings.delete(id._id).then(function(response){console.log('Deleted')},
+      Items.delete(id._id).then(function(response){console.log('Deleted')},
       function(error){console.log('error')}
     );
-      $scope.listings.splice(index, 1);
+      $scope.items.splice(index, 1);
 
     };
 
     /** DONE WITH deleteListing **/
 
     $scope.showDetails = function(index) {
-      $scope.detailedInfo = $scope.listings[index];
+      $scope.detailedInfo = $scope.items[index];
     };
+
+    $scope.addToCart = function(index) {
+      /** TODO Cart function
+       */
+    }
   }
 ]);
