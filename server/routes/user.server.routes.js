e8c6df83b8adf205478b505459972c334e986755
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-26 13:03:55 -0400

    user registration, user login in progress

---
+++ server/routes/user.server.routes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+// Bring in User Model
+let User = require('../models/user.server.model');
+
+// Register Form
+router.get('/register', function(req, res){
+  res.render('register');
+});
+
+// Register Proccess
+router.post('/register', function(req, res){
+  const username = req.body.username;
+  const email = req.body.email;
+  const userType = req.body.userType;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('userType', 'User Type is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors:errors
+    });
+  } else {
+    let newUser = new User({
+      username:username,
+      email:email,
+	  userType:userType,
+      password:password
+    });
+
+	// once here, newUser is created by still need to hash the pw
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success','You are now a registered user and can log in!');
+            res.redirect('/login');
+          }
+        });
+      });
+    });
+  }
+});
+
+module.exports = router;
