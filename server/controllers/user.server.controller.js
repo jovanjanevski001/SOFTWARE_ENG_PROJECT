2754e31929f56359e0c371fbc24ea2db96f8e433
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 11:47:36 -0400

    changes for user regis6.

---
+++ server/controllers/user.server.controller.js
@@ -0,0 +1,66 @@
+/* Dependencies */
+var mongoose = require('mongoose'),
+    User = require('../models/user.server.model.js');
+
+/*
+  In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
+  On an error you should send a 404 status code, as well as the error message.
+  On success (aka no error), you should send the listing(s) as JSON in the response.
+  HINT: if you are struggling with implementing these functions, refer back to this tutorial
+  from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+ */
+
+/* Create a listing */
+exports.create = function(req, res) {
+
+  /* Instantiate a Listing */
+  var user = new User(req.body);
+
+
+  /* Then save the listing */
+  user.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(user);
+    }
+  });
+};
+
+/* Update a listing */
+exports.update = function(req, res) {
+  var user = req.user;
+
+  // change the contents of listing to the req's body contents
+  user.name = req.body.name;
+  user.price= req.body.price;
+  user.vendor= req.body.vendor;
+
+  /* Save the listing */
+  user.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else {
+      res.json(user);
+    }
+  });
+};
+
+/*
+  Middleware: find a listing by its ID, then pass it to the next request handler.
+  HINT: Find the listing using a mongoose query,
+        bind it to the request object as the property 'listing',
+        then finally call next
+ */
+exports.userByID = function(req, res, next, id) {
+  User.findById(id).exec(function(err, user) {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      req.user = user;
+      next();
+    }
+  });
+};
