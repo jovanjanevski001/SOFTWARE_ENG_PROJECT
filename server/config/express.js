b7a84d91007ae0d6f7e535d9cf8e85ec7100402f
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 09:24:06 -0400

    changes for user regis6.

--- server/config/express.js
+++ server/config/express.js
@@ -41,7 +41,7 @@ mongoose.connect(config.db.uri, {useMongoClient: true});
   /**TODO
   Use the listings router for requests to the api */
   app.use('/api/items', itemsRouter);
-  app.use('/cr', usersRouter); 
+  app.use('/api/users', usersRouter); 
 
   /**TODO
   Go to homepage for all routes not specified */
--- server/routes/user.server.route.js
+++ server/routes/user.server.route.js
@@ -1,4 +1,47 @@
-// User model
+/* Dependencies */
+var users = require('../controllers/item.server.controller.js'),
+    express = require('express'),
+    router = express.Router();
+
+/*
+  These method calls are responsible for routing requests to the correct request handler.
+  Take note that it is possible for different controller functions to handle requests to the same route.
+ */
+
+router.route('/')
+  .get(users.list)
+  .post(users.create);
+
+
+/*
+  The ':' specifies a URL parameter.
+ */
+router.route('/:userId')
+  .get(users.read)
+  .put(users.update)
+  .delete(users.delete);
+
+/*
+  The 'router.param' method allows us to specify middleware we would like to use to handle
+  requests with a parameter.
+
+  Say we make an example request to '/listings/566372f4d11de3498e2941c9'
+
+  The request handler will first find the specific listing using this 'listingsById'
+  middleware function by doing a lookup to ID '566372f4d11de3498e2941c9' in the Mongo database,
+  and bind this listing to the request object.
+
+  It will then pass control to the routing function specified above, where it will either
+  get, update, or delete that specific listing (depending on the HTTP verb specified)
+ */
+router.param('userId', users.userByID);
+
+module.exports = router;
+
+
+
+
+/* // User model
 var users = require('../models/user.server.model');
 
 const express = require('express');
@@ -59,4 +102,4 @@ router.post('cr', function(req, res){
 });
 
 
-module.exports = router;
+module.exports = router; */
