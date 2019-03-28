2aa1691dafb4cef8c350ea0be3b8b0d780a243c5
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 14:07:03 -0400

    working on user registration

--- client/js/factories/userFactory.js
+++ client/js/factories/userFactory.js
@@ -1,11 +1,11 @@
 angular.module('users', []).factory('Users', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('/users');
+      return $http.get('/api/users');
     },
 
 	create: function(user) {
-	  return $http.post('/users', user);
+	  return $http.post('/api/users', user);
     }
 
   };
