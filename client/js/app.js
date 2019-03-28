0ee65d64945a526e8665c555359a20eebb754334
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 10:34:21 -0400

    changes for user regis6.

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,5 @@
 /* register the modules the application depends upon here*/
-angular.module('items', []);
+angular.module('users', []);
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('directoryApp', ['items']);
+var app = angular.module('directoryApp', ['users']);
