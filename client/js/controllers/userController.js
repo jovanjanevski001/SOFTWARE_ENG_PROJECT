7595c96cf91c7d4388222956e23f2647f894cb9f
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 13:09:19 -0400

    changes for user regis6.

--- client/js/controllers/userController.js
+++ client/js/controllers/userController.js
@@ -34,18 +34,5 @@ angular.module('users').controller('UserController', ['$scope', 'Users',
       function(response){$scope.newUser.password=''; $scope.newUser.password2='';}
     }
     };
-
-    $scope.deleteUser = function(index, id) {
-	   /**TODO
-        Delete the article using the Listings factory. If the removal is successful,
-		navigate back to 'listing.list'. Otherwise, display the error.
-       */
-
-
-
-      Users.delete(id);
-      $scope.users.splice(index, 1);
-
-    };
   }
 ]);
--- server/routes/user.server.routes.js
+++ server/routes/user.server.routes.js
@@ -13,29 +13,6 @@ router.route('/')
   .post(users.create);
 
 
-/*
-  The ':' specifies a URL parameter.
- */
-router.route('/:userId')
-  .get(users.read)
-  .put(users.update)
-  .delete(users.delete);
-
-/*
-  The 'router.param' method allows us to specify middleware we would like to use to handle
-  requests with a parameter.
-
-  Say we make an example request to '/listings/566372f4d11de3498e2941c9'
-
-  The request handler will first find the specific listing using this 'listingsById'
-  middleware function by doing a lookup to ID '566372f4d11de3498e2941c9' in the Mongo database,
-  and bind this listing to the request object.
-
-  It will then pass control to the routing function specified above, where it will either
-  get, update, or delete that specific listing (depending on the HTTP verb specified)
- */
-router.param('userId', users.userByID);
-
 module.exports = router;
 
 
