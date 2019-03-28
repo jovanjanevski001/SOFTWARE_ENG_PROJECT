f237872e3fe1e1fe24a585c73c76f698963c64c4
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-28 10:43:51 -0400

    changes for user regis6.

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,7 @@ var path = require('path'),
     bodyParser = require('body-parser'),
     config = require('./config'),
     itemsRouter = require('../routes/item.server.routes'),
-	usersRouter= require('../routes/user.server.route');
+	//usersRouter= require('../routes/user.server.route');
 
 module.exports.init = function() {
   //connect to database
@@ -41,7 +41,7 @@ mongoose.connect(config.db.uri, {useMongoClient: true});
   /**TODO
   Use the listings router for requests to the api */
   app.use('/api/items', itemsRouter);
-  app.use('/api.users' usersRouter); 
+  //app.use('/api.users' usersRouter); 
 
   /**TODO
   Go to homepage for all routes not specified */
