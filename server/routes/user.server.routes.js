226223085688f2ffebfecf8bdf5da14aa8bbdca7
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-03-29 16:12:05 -0400

    working on user registration

--- server/routes/user.server.routes.js
+++ server/routes/user.server.routes.js
@@ -8,7 +8,7 @@ var users = require('../controllers/user.server.controller.js'),
   Take note that it is possible for different controller functions to handle requests to the same route.
  */
 
-router.route('/')
+router.route('/cr?')
   .post(users.create);
 
 
