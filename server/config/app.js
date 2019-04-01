1689122007401eabfc6b9c906d6a891eaaff745e
jovanjanevski001 <32399896+jovanjanevski001@users.noreply.github.com>
2019-04-01 15:19:24 -0400

    testing

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,7 @@ var config = require('./config'),
     express = require('./express');
 
 module.exports.start = function() {
+  // testing
   var app = express.init();
   app.listen(process.env.PORT || config.port, function() {
     console.log('App listening on port', process.env.PORT || config.port);
