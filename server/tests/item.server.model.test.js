8010bc77013a8b1b72b1c40cf85a957fc8df473d
Andrew Sanchez <andrewfsanchez@gmail.com>
2019-04-21 23:45:47 -0400

    added some basic testing

---
+++ server/tests/item.server.model.test.js
@@ -0,0 +1,83 @@
+var should = require('should'),
+    mongoose = require('mongoose'),
+    Item = require('../models/listings.server.model'),
+    User = require('../models/user.server.model'),
+    config = require('../config/config');
+
+var listing, id;
+
+item =  {
+  name: "test",
+  price: 2,
+  vendor: "test vendor"
+}
+
+describe('Item Schema Unit Tests', function() {
+
+  before(function(done) {
+    mongoose.connect(config.db.uri);
+    done();
+  });
+
+  describe('Saving to database', function() {
+    /*
+      Mocha's default timeout for tests is 2000ms. To ensure that the tests do not fail
+      prematurely, we can increase the timeout setting with the method this.timeout()
+     */
+    this.timeout(10000);
+
+    it('saves properly when name, price, and vendor provided', function(done){
+      new Item({
+        name: item.name,
+        price: item.price,
+        vendor: item.vendor
+      }).save(function(err, item){
+        should.not.exist(err);
+        id = item._id;
+        done();
+      });
+    });
+
+    it('throws an error when name not provided', function(done){
+      new Listing({
+        price: item.price,
+        vendor: item.vendor
+      }).save(function(err){
+        should.exist(err);
+        done();
+      })
+    });
+
+    it('throws an error when vendor not provided', function(done){
+      new Listing({
+        name: item.name,
+        price: item.price
+      }).save(function(err){
+        should.exist(err);
+        done();
+      })
+    });
+
+    it('throws an error when price not provided', function(done){
+      new Listing({
+        name: item.name,
+        vendor: item.vendor
+      }).save(function(err){
+        should.exist(err);
+        done();
+      })
+    });
+
+  });
+
+  afterEach(function(done) {
+    if(id) {
+      Listing.remove({ _id: id }).exec(function() {
+        id = null;
+        done();
+      });
+    } else {
+      done();
+    }
+  });
+});
